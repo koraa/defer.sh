42e695308004947fa5dba3828c207f1984402ef7
Karolin Varner <karo@cupdev.net>
2013-09-12 02:13:04 +0200

    Use startf but as 3rd arg

--- defer.sh
+++ defer.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-# TODO: Options: nofork, tmpdir, signature, tmpfile, hashf, abort, query, accumulate, frompipe, clear, defer-block
-# TODO: Expire deferance
 
 test -n "$TMP" || TMP="$TEMPDIR"
 test -n "$TMP" || TMP="$TEMP"
 test -n "$TMP" || TMP=/tmp
 
 time="$1"
-startf="$2"
-endf="$3"
+endf="$2"
+startf="$3"
 
 id="`shasum <<< "$0 $@" | awk '{print $1}'`"
 tf="$TMP/defer-$id.tmp"
